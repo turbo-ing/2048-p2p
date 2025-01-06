7a5add84c803f531475254425e64f73fe23ffdb0
Alexander Westlake <alex@turbo.ing>
2025-01-06 15:45:18 +0000

    Move zk waiting modal to after results page

--- src/app/components/ResultModal.tsx
+++ src/app/components/ResultModal.tsx
@@ -34,114 +34,151 @@ export const ResultModal = ({
 }: ResultModalProps) => {
   const router = useRouter();
   const [ranking, _setRanking] = useState<Player[]>(rankingData);
+  const [download, setDownload] = useState<boolean>(false);
   const playerCount = ranking.length;
 
   return (
-    <Modal show={open}>
-      {lenQueue === 0 && (
-        <div>
-          <div className="relative flex justify-center items-center">
-            <img
-              alt=""
-              src={isWinner ? "/svg/2048-winner.svg" : "/svg/2048-loser.svg"}
-            />
-          </div>
-          <div className="py-6 text-center">
-            <div className="text-[#F5F5F6] font-semibold text-3xl">
-              {playerCount > 1 &&
-                (player == ranking[0].name
-                  ? "You've won the match!"
-                  : "You've been beaten!")}
-              {playerCount < 2 && (isWinner ? "You win!" : "Game over!")}
+    <div>
+      <Modal show={open}>
+        {!download && (
+          <div>
+            <div className="relative flex justify-center items-center">
+              <img
+                alt=""
+                src={isWinner ? "/svg/2048-winner.svg" : "/svg/2048-loser.svg"}
+              />
             </div>
-            <div className="text-xl my-4">
-              <p className="text-center text-2xl mb-2 font-bold">
-                {playerCount > 1 && "Ranking"}
-              </p>
-              <ul className="counter-list">
+            <div className="py-6 text-center">
+              <div className="text-[#F5F5F6] font-semibold text-3xl">
                 {playerCount > 1 &&
-                  ranking.map((player) => (
-                    <li
-                      key={player.name}
-                      className="flex justify-between relative px-5 mb-2 last:mb-0"
-                    >
-                      <p>{player.name}</p>
-                      <p>{player.score}</p>
-                    </li>
-                  ))}
-                {playerCount < 2 && "Score: " + ranking[0].score}
-              </ul>
-            </div>
-            <div className="mt-3 text-[#94969C] font-medium text-base">
-              {playerCount > 1 &&
-                (player == ranking[0].name
-                  ? "Congratulations! Your strategy and skill have prevailed. Well played!"
-                  : "Good effort! Learn from this match and come back stronger. Better luck next time!")}
-              {playerCount < 2 &&
-                (isWinner
-                  ? "You're officially a 2048 master!"
-                  : "Better luck next time!")}
-            </div>
-          </div>
-          <div className="mt-6 flex gap-3">
-            <button
-              className="rounded-full py-2.5 px-4 border border-[#D0D5DD] bg-white text-[#344054] text-base font-semibold gap-1.5 flex items-center justify-center w-1/3"
-              onClick={() => leave()}
-            >
-              <img alt="" src="/svg/home.svg" />
-              <div>Home</div>
-            </button>
-            <button
-              className="rounded-full py-2.5 px-4 border border-[#D0D5DD] bg-white text-[#344054] text-base font-semibold gap-1.5 flex items-center justify-center w-full"
-              onClick={() => downloadProof()}
-            >
-              <div>Download ZK Proof</div>
-            </button>
-            <button
-              className="rounded-full py-2.5 px-4 bg-[#F23939] text-white text-base font-semibold gap-1.5 flex items-center justify-center w-2/3"
-              onClick={() => {
-                router.push("/");
-              }}
-            >
-              <img alt="" src="/svg/repeat.svg" />
-              <div>
-                {playerCount < 2 && "Play again"}
-                {playerCount > 1 && "Rematch"}
+                  (player == ranking[0].name
+                    ? "You've won the match!"
+                    : "You've been beaten!")}
+                {playerCount < 2 && (isWinner ? "You win!" : "Game over!")}
               </div>
-            </button>
-          </div>
-        </div>
-      )}
-      {lenQueue !== 0 && (
-        <div>
-          <div className="relative flex justify-center items-center">
-            <img alt="" src={"/svg/circle-plus.svg"} />
-          </div>
-          <div className="py-6 text-center">
-            <div className="text-[#F5F5F6] font-semibold text-3xl">
-              Generating ZK Proof... Moves left to process: {lenQueue}
-            </div>
-            <div className="text-xl my-4">
-              <p className="text-center text-2xl mb-2 font-bold">
-                {playerCount > 1 && "Ranking"}
-              </p>
-              <ul className="counter-list">
+              <div className="text-xl my-4">
+                <p className="text-center text-2xl mb-2 font-bold">
+                  {playerCount > 1 && "Ranking"}
+                </p>
+                <ul className="counter-list">
+                  {playerCount > 1 &&
+                    ranking.map((player) => (
+                      <li
+                        key={player.name}
+                        className="flex justify-between relative px-5 mb-2 last:mb-0"
+                      >
+                        <p>{player.name}</p>
+                        <p>{player.score}</p>
+                      </li>
+                    ))}
+                  {playerCount < 2 && "Score: " + ranking[0].score}
+                </ul>
+              </div>
+              <div className="mt-3 text-[#94969C] font-medium text-base">
                 {playerCount > 1 &&
-                  ranking.map((player) => (
-                    <li
-                      key={player.name}
-                      className="flex justify-between relative px-5 mb-2 last:mb-0"
-                    >
-                      <p>{player.name}</p>
-                      <p>{player.score}</p>
-                    </li>
-                  ))}
-                {playerCount < 2 && "Score: " + ranking[0].score}
-              </ul>
+                  (player == ranking[0].name
+                    ? "Congratulations! Your strategy and skill have prevailed. Well played!"
+                    : "Good effort! Learn from this match and come back stronger. Better luck next time!")}
+                {playerCount < 2 &&
+                  (isWinner
+                    ? "You're officially a 2048 master!"
+                    : "Better luck next time!")}
+              </div>
+            </div>
+            <div className="mt-6 flex gap-3">
+              <button
+                className="rounded-full py-2.5 px-4 border border-[#D0D5DD] bg-white text-[#344054] text-base font-semibold gap-1.5 flex items-center justify-center w-1/3"
+                onClick={() => leave()}
+              >
+                <img alt="" src="/svg/home.svg" />
+                <div>Home</div>
+              </button>
+              <button
+                className="rounded-full py-2.5 px-4 border border-[#D0D5DD] bg-white text-[#344054] text-base font-semibold gap-1.5 flex items-center justify-center w-full"
+                onClick={() => setDownload(true)} //downloadProof()
+              >
+                <div>Download ZK Proof</div>
+              </button>
+              <button
+                className="rounded-full py-2.5 px-4 bg-[#F23939] text-white text-base font-semibold gap-1.5 flex items-center justify-center w-2/3"
+                onClick={() => {
+                  router.push("/");
+                }}
+              >
+                <img alt="" src="/svg/repeat.svg" />
+                <div>
+                  {playerCount < 2 && "Play again"}
+                  {playerCount > 1 && "Rematch"}
+                </div>
+              </button>
             </div>
           </div>
-        </div>
-      )}
-    </Modal>
+        )}
+        {download && lenQueue === 0 && (
+          <div>
+            <div className="relative flex justify-center items-center">
+              <img alt="" src={"/svg/circle-plus.svg"} />
+            </div>
+            <div className="py-6 text-center">
+              <div className="text-[#F5F5F6] font-semibold text-3xl">
+                Download ZK Proof
+              </div>
+              <div className="relative flex justify-center items-center">
+                <button
+                  className="rounded-full m-5 py-2.5 px-4 border border-[#D0D5DD] bg-white text-[#344054] text-base font-semibold gap-1.5 flex items-center justify-center w-2/5"
+                  onClick={() => downloadProof()}
+                >
+                  <div>Download Proof</div>
+                </button>
+                <button
+                  className="rounded-full m-5 py-2.5 px-4 border border-[#D0D5DD] bg-white text-[#344054] text-base font-semibold gap-1.5 flex items-center justify-center w-2/5"
+                  onClick={() => setDownload(false)} //downloadProof()
+                >
+                  <div>Back to Results</div>
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+        {download && lenQueue !== 0 && (
+          <div>
+            <div className="relative flex justify-center items-center">
+              <img alt="" src={"/svg/circle-plus.svg"} />
+            </div>
+            <div className="py-6 text-center">
+              <div className="text-[#F5F5F6] font-semibold text-3xl">
+                Generating ZK Proof... Moves left to process: {lenQueue}
+              </div>
+              <div className="relative flex justify-center items-center">
+                <button
+                  className="rounded-full mt-4 py-2.5 px-4 border border-[#D0D5DD] bg-white text-[#344054] text-base font-semibold gap-1.5 flex items-center justify-center"
+                  onClick={() => setDownload(false)} //downloadProof()
+                >
+                  <div>Back to Results</div>
+                </button>
+              </div>
+              <div className="text-xl my-4">
+                <p className="text-center text-2xl mb-2 font-bold">
+                  {playerCount > 1 && "Ranking"}
+                </p>
+                <ul className="counter-list">
+                  {playerCount > 1 &&
+                    ranking.map((player) => (
+                      <li
+                        key={player.name}
+                        className="flex justify-between relative px-5 mb-2 last:mb-0"
+                      >
+                        <p>{player.name}</p>
+                        <p>{player.score}</p>
+                      </li>
+                    ))}
+                  {playerCount < 2 && "Score: " + ranking[0].score}
+                </ul>
+              </div>
+            </div>
+          </div>
+        )}
+      </Modal>
+    </div>
   );
 };
